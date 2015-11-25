1869c2b3c480cc1b57270ed22dc0e71847e1cf4f
= <lrbabe@gmail.com>
2015-11-25 19:28:58 +0100

    debug appback 1

--- hooks/handlers/webhooksRequest.js
+++ hooks/handlers/webhooksRequest.js
@@ -3,31 +3,31 @@ var utils = require('../../lib/utils');
 module.exports = function handleWebhooksRequest( hoodie, request, reply ) {
 	var event = request.payload;
 	var usersDb = hoodie.database('_users');
-
+console.log(1);
 	// For now we will ignore all events except subscriptions
 	if ( !event.type || !/^customer.subscription/.test( event.type ) ) {
 		return reply( null, 'event ignored' );
 	}
-
+console.log(2);
 	var customerId = event.data.object.customer;
 	var queryArgs = {
 		'include_docs': true,
 		startkey: customerId,
 		limit: 1,
 	};
-
+console.log(3);
 	usersDb.query('stripe-by-id', queryArgs, function(error, rows) {
 		if ( error ) {
 			return reply(new Error(error));
 		}
-
+console.log(4);
 		var username = rows[0].id.split('/')[1];
 
 		hoodie.account.find('user', username, function(error, userDoc) {
 			if (error) {
 				return reply(new Error(error));
 			}
-
+console.log(5);
 			if ( event.type === 'customer.subscription.deleted' ) {
 				userDoc.stripe.plan = 'free_none';
 				delete userDoc.stripe.subscriptionId;
@@ -42,7 +42,7 @@ module.exports = function handleWebhooksRequest( hoodie, request, reply ) {
 				if (error) {
 					return reply(new Error(error));
 				}
-
+console.log(6);
 				reply( null, 'success' );
 			});
 		});
